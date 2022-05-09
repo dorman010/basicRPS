694a6cdab748ca74a0ce425774318d5cc5309679
Uria <dorman010@gmail.com>
2022-05-09 21:59:37 +0100

    added buttons UI, start of JS

--- script.js
+++ script.js
@@ -1,32 +1,18 @@
+let payerScore = 0
+let compputerScore = 0
+const buttons = document.querySelector('input')
+
+
 //Computer random choice:
 function computerPlay() {
     let choices = ['rock', 'paper', 'scissors']
     return choices[Math.floor(Math.random() * choices.length)]
 }
+computerPlay()
 
-//Checks selections and returns winner
-function playRound (playerSelection, computerSelection) {
-    if (playerSelection =='rock' && computerSelection == 'scissors' || playerSelection == 'paper' && computerSelection == 'rock' || playerSelection == 'scissors' && computerSelection == 'paper') {
-        return `You win! ${playerSelection} beats ${computerSelection}.`}
-    else if (playerSelection =='rock' && computerSelection =='paper' || playerSelection == 'paper' && computerSelection == 'scissors' || playerSelection == 'scissors' && computerSelection == 'rock') {
-        return `You lose! ${computerSelection} beats ${playerSelection}`}
-    else if (playerSelection == computerSelection) {
-        return `Tie! You both picked ${playerSelection}.`
-    }
-    else {
-        return `Invalid choice`}
-    }
-// player input case insensitive, iterates through the game 5 times, logs round number and also selections made.
-function game(){
-    for (let i = 0; i<5; i++) {
-        let computerSelection = computerPlay();
-        let playerSelection = prompt('Rock, Paper or Scissors?')
-        playerSelection = playerSelection.toLowerCase();
-        console.log(`You selected: ${playerSelection}!`)
-        console.log(`Computer selected: ${computerSelection}!`)
-        console.log(`Round ${i+1}: ` + playRound(playerSelection, computerSelection))
-    }
-
+// disable buttons at the end of the game:
+function disableButtons() {
+    buttons.forEach(elem => {
+        elem.disabled = true;
+    })
 }
-
-game();
